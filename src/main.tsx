744100833cad4929cbcc9be44ed9bb9dcf5a920d
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-08-13 16:14:35 -0400

    Build a nav bar

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <DevSupport
+        ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+      >
+        <App />
+      </DevSupport>
     </ChakraProvider>
   </React.StrictMode>,
 );
