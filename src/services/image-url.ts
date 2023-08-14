a7498fa1d8ccb133bf67dc25865a5b709852d5fb
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-08-14 08:07:37 -0400

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,13 @@
+const getCroppedImageUrl = (url: string) => {
+  const target = "media/";
+  // the images coming in are big, and we don't want to download them just
+  // to display a thumbnail
+  // the API provides a parameter to rop
+  // we want to crop the images using the api crop parameter
+  // find where we start the media parameter so we can add crop
+  const index = url.indexOf(target) + "media/".length;
+  // we want to insert the crop parameter in the request URI
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+};
+
+export default getCroppedImageUrl;
