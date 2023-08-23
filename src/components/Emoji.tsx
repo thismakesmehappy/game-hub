a2674c3166a56cba11f3f0da20ab0d145fc58e7e
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-08-23 09:10:00 -0400

    Add emojis

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,26 @@
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+export function Emoji({ rating }: Props) {
+  if (rating < 3) return null;
+
+  // These are the props for an Image object, so they get spread in the object
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh", boxSize: "25px" },
+    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+  };
+  return (
+    <Image
+      src={emojiMap[rating].src}
+      alt={emojiMap[rating].alt}
+      {...emojiMap[rating]}
+      marginTop={1}
+    />
+  );
+}
